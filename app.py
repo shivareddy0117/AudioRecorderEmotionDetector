bdc512967029bb5df2b74f2e37904e870228e2fd
huxiaomeng <gregH@users.noreply.huggingface.co>
2023-06-29 01:04:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def detect(text):
           }
     return res
 
-image = gr.Textbox(label ="Text to be detected",info ="Disclaimer: The detector is not perfect. It may lead to incorrect detections (see examples below).")
+image = gr.Textbox(label ="Text to be detected")
 label = gr.Label(label ="Prediction")
 AI_texts = [
     "Greetings I would like to express my utmost gratitude for the extraordinary experience I had while visiting our neighboring planet Mars. It has been a decade since I last scrutinized Mars' features and the information gathered during my journey has revitalized my fascination for this captivating planet. Despite the technological advancements in Mars exploration I was still able to witness the landscape in its natural state capturing breathtaking photographs and filming videos that showcased the red planet's impressive terrain fascinating geology and intriguing scientific discoveries. This experience has forever changed my perception of humanity and inspired my passion for space exploration hoping to further contribute to our understanding of the universe and our place within it.",
@@ -49,6 +49,7 @@ with gr.Interface(fn=detect, inputs=image, outputs=label, title=title, descripti
     with gr.Tab("Human-text examples"):
         gr.Examples(Human_texts, inputs=image, outputs=label)
     with gr.Tab("Examples of incorrect detections"):
-        gr.Examples(Constitution_inputs, inputs=image, outputs=label)
+        gr.Examples(Constitution_inputs, inputs=gr.Textbox(label ="Text to be detected",info ="Disclaimer: The detector is not perfect. It may lead to incorrect detections (see examples below).")
+, outputs=label)
 
 intf.launch(inline=False)
