4f355a1fea6a33a0e20343cfb2465622012653cd
huxiaomeng <gregH@users.noreply.huggingface.co>
2023-07-11 03:47:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -50,15 +50,15 @@ with gr.Interface(fn=detect, inputs=image, outputs=label, title=title, descripti
         gr.Examples(Human_texts, inputs=image, outputs=label)
     with gr.Tab("Examples of incorrect detections"):
         gr.Examples(Constitution_inputs, inputs=image, outputs=label)
-    citation = r"If you use this detector, please cite the following paper: "+"\n"+
-           "```bash"+"\n"+"@misc{hu2023radar,"+"\n"+ 
-           r"      title={RADAR: Robust AI-Text Detection via Adversarial Learning},"+"\n" +
-           r"      author={Xiaomeng Hu and Pin-Yu Chen and Tsung-Yi Ho},"+"\n" +
-           r"      year={2023},"+"\n" +
-           r"      eprint={2307.03838},"+"\n" +
-           r"      archivePrefix={arXiv},"+"\n" +
-           r"      primaryClass={cs.CL}"+"\n" +
-           "}```"
+    citation = r"If you use this detector, please cite the following paper: "+"\n"+ \
+               "```bash"+"\n"+"@misc{hu2023radar,"+"\n"+ \
+               r"      title={RADAR: Robust AI-Text Detection via Adversarial Learning},"+"\n" + \
+               r"      author={Xiaomeng Hu and Pin-Yu Chen and Tsung-Yi Ho},"+"\n" + \
+               r"      year={2023},"+"\n" + \
+               r"      eprint={2307.03838},"+"\n" + \
+               r"      archivePrefix={arXiv},"+"\n" + \
+               r"      primaryClass={cs.CL}"+"\n" + \
+               "}```"
     gr.Markdown(citation)
 
 intf.launch(inline=False)
