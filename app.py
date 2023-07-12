3dc3156c6b520271edd3dd37f0d2592b6e704dcc
huxiaomeng <gregH@users.noreply.huggingface.co>
2023-07-12 07:27:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ Constitution_inputs = [
     "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defense, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America."
 ]
 title = "RADAR: Robust AI-text Detection via Adversarial leanRning"
-description = "An AI-written text detector based on the roberta-large model (fine-tuned with Vicuna-7B). See https://arxiv.org/abs/2307.03838 for details.\nProject Page: https://radar.vizhub.ai/"
+description = "An AI-written text detector based on the roberta-large model (fine-tuned with Vicuna-7B).  See https://arxiv.org/abs/2307.03838 for details.  Project Page: https://radar.vizhub.ai/"
 
 intf = gr.Interface(fn=detect, inputs=image, outputs=label, title=title,
                     description=description)
