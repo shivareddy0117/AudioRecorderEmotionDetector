e491d815602c43fd34fd17a8ad129ab6cc990ce8
ShivaReddy Mogili <113395143+shivareddy0117@users.noreply.github.com>
2023-07-29 12:13:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 <script>
+document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('jobApplicationForm').addEventListener('submit', function(e) {
-        e.preventDefault(); // prevent the form from submitting
-        
-        // Now you can send form data to your server using AJAX here if needed.
-        
-        // Display thank you message
+        e.preventDefault();
         alert('Thank you for the application!');
-        
-        // Optionally, clear the form fields
         this.reset();
     });
+});
 </script>
