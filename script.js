987346d21958c9afc8b90abe701d5a7e82a22137
ShivaReddy Mogili <113395143+shivareddy0117@users.noreply.github.com>
2024-04-16 15:34:23 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('jobApplicationForm').addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert('Thank you for the application!');
-        this.reset();
-    });
-});
-</script>
